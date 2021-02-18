f6208974bc2ecfee71d4cb264d0a7fc8b50b8388
Silvy <silveee98@gmail.com>
2021-02-18 14:55:54 +0530

    fix typo in db.js: process.DB_USER/PASS -> process.env.DB_USER/PASS

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 const connection = MongoClient.connect(
 	format(
 		'mongodb://%s:%s@%s:%s/?authMechanism=%s&authSource=%s',
-		process.DB_USER, process.DB_PASS, process.env.DB_HOST, process.env.DB_PORT,
+		process.env.DB_USER, process.env.DB_PASS, process.env.DB_HOST, process.env.DB_PORT,
 		process.env.DB_AUTH_MECHANISM, process.env.DB_NAME,
 	),
 	{
