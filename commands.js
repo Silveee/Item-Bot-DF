3bf90f597d5b689813feaba79908448918262cbd
Silvy <silveee98@gmail.com>
2021-04-13 20:22:00 +0530

    move common functions/objects into utils.js

--- commands.js
+++ commands.js
@@ -1,8 +1,9 @@
 'use strict';
 
+const { bonuses, capitalize, formatTag, formatBoosts, sanitizeText, validTypes } = require('./utils');
+
 const connect = require('./db');
 
-const CT = process.env.COMMAND_TOKEN;
 const aliases = {
 	'nsod': 'necrotic sword of doom',
 	'boa': 'blade of awe',
@@ -46,111 +47,8 @@ const aliases = {
 	'your mom': 'unsqueakable farce',
 	'ur mom': 'unsqueakable farce',
 };
-const validTypes = new Set([
-	'weapon', 'accessory', 'cape', 'wing', 'helm',
-	'ring', 'belt', 'necklace', 'trinket', 'bracer'
-]);
-const bonuses = new Set([
-	'block', 'dodge', 'parry', 'crit', 'magic def', 'pierce def', 'melee def',
-	'wis', 'end', 'cha', 'luk', 'int', 'dex', 'str', 'bonus'
-]);
 
-/**
- * Capitalizes the first letter of every other word in the input text, with
- * few exceptions, which are instead capitalized fully.
- *
- * @param {String} text
- *   Text to be capitalized
- *
- * @return {String}
- *   String with alternate words in the input text capitalized, or the text
- *   fully capitalized if the text is one of several values
- */
-function capitalize(text) {
-	const fullCapWords = new Set([ // These words are fully capitalized
-		'str', 'int', 'dex', 'luk', 'cha', 'dm', 'fs',
-		'wis', 'end', 'dm', 'so', 'dc', 'da', 'ak'
-	]);
-	if (fullCapWords.has(text)) return text.toUpperCase();
-
-	if (!text || !text.trim()) return text;
-
-	return text
-		.trim()
-		.split(' ')
-		.map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
-}
-
-/**
- * Format an input tag
- *
- * @param {String} tag
- *   Tag to be formatted
- *
- * @return {String}
- *   'Seasonal', 'ArchKnight Saga', 'Alexander Saga' if the input tag is 'se', 'ak', or 'alexander'
- *   respectively. The return value is capitalize(tag) otherwise
- */
-function formatTag(tag) {
-	return {
-		'se': 'Seasonal',
-		'ak': 'ArchKnight Saga',
-		'alexander': 'Alexander Saga',
-		'temp': 'Temporary',
-		'fs': 'Free Storage'
-	}[tag] || capitalize(tag);
-}
-
-/**
- * Does the following:
- * - Lowercases input text
- * - Removes leading and trailing whitespace
- * - Removes accents from input text characters
- * - Removes brackets, quotes, and backticks
- * - Replaces all other alphanumeric characters with a single whitespace
- * - Replaces all instances of more than one whitespace with a single whitespace
- * Example: "Alina's Battle-Bouquet Staff" -> "alinas battle bouquet staff"
- *
- * @param {String} text
- *   Input text to be sanitized
- *
- * @return {String}
- *   Sanitized text
- */
-function sanitizeText(text) {
-	return text.toLowerCase()
-		// Replace accented characters with their non-accented versions
-		.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-		.replace(/[()'"“”‘’`]/g, '') // Remove brackets, quotes, and backticks
-		// Replace all other non-alphanumeric character (other than |)
-		// sequences with a single whitespace
-		.replace(/[^a-z0-9|]+/g, ' ')
-		.trim();
-}
-
-/**
- * Formats an array of boosts (resistances or stat bonuses) into a comma-separated list
- * Example: [{ k: 'int', v: 3 }, { k: 'melee def', v: -1 }] -> 'INT +3, Melee Def -1'
- * 
- * @param {{ k: String, v: Number }[] | null} boosts
- *   An array of objects listing each boost's name (k) and value (v)
- *
- * @return {String}
- *   A comma separated list of boost names and their corresponding values, obtained by:
- *   - Capitalizing each boost name
- *   - Prepending a '+' to the boost's value if it is > 0
- *   - Joining boost's name and value together with a space
- *   - Joining each string of boost name/value pairs with ', '
- */
-function formatBoosts(boosts) {
-	return (boosts || [])
-		.map(boost => {
-			const name = capitalize(boost.k);
-			const value = (boost.v < 0 ? '' : '+') + boost.v;
-			return name + ' ' + value;
-		})
-		.join(', ') || 'None';
-}
+const CT = process.env.COMMAND_TOKEN;
 
 /**
  * Get details of an item from the database. The input item name is sanitized and converted to its original form
@@ -384,23 +282,6 @@ exports.commands = {
 		if (bonuses.has(sortExp)) sortExp = 'bonuses.' + sortExp;
 		else if (sortExp !== 'damage') sortExp = 'resists.' + sortExp;
 
-		/* Keeps lower level items but removes items with the same name or pedia url
-		   within the same group. This block of code might be needed later. */
-		// const pipeline = [
-		// 	{ $addFields: { damage: { $avg: '$damage' }, bonuses: { $arrayToObject: '$bonuses' }, resists: { $arrayToObject: '$resists' } } },
-		// 	{ $addFields: { newField: '$' + sortExp } },
-		// 	{ $match: filter },
-
-		// 	{ $sort: { level: -1 } },
-		// 	// Remove documents that have the same name and newly added field value, keep only max level version
-		// 	{ $group: { _id: { newField: '$newField', name: '$name' }, doc: { $first: '$$CURRENT' } } },
-		// 	// Remove documents that have the same pedia url and newly added field value
-		// 	{ $group: { _id: { newField: '$doc.newField', link: '$doc.link' }, doc: { $first: '$doc' } } },
-		// 	// Place all items with the same new field value into the same group
-		// 	{ $group: { _id: '$doc.newField', newField: { $first: '$doc.newField' }, items: { $addToSet: { title: '$doc.title', level: '$doc.level', tags: '$doc.tags' } } } },
-		// 	{ $sort: { newField: sortOrder } },
-		// 	{ $limit: 10 }
-		// ];
 		const pipeline = [
 			{
 				$addFields: {
--- expression-evaluator.js
+++ expression-evaluator.js
@@ -1,16 +1,13 @@
 'use strict';
 
+const { bonuses, capitalize, isResist } = require('./utils');
+
 const MAX_OPERATORS = 5;
 const OPERATORS = {
 	'+': { precedence: 1, unary: false, mongoFunc: '$add' },
 	'-': { precedence: 1, unary: false, mongoFunc: '$subtract' },
 	'u-': { precedence: 2, unary: true, mongoFunc: '$subtract' }
 };
-
-const bonuses = new Set([
-	'block', 'dodge', 'parry', 'crit', 'magic def', 'pierce def', 'melee def',
-	'wis', 'end', 'cha', 'luk', 'int', 'dex', 'str', 'bonus'
-]);
 
 class InvalidTokenInExpressionError extends Error {}
 
@@ -24,37 +21,6 @@ class TooManyOperatorsInExpressionError extends Error {
 	constructor() {
 		super(`Your sort expression cannot have more than ${MAX_OPERATORS} operators in it.`);
 	}
-}
-
-function isResist(value) {
-	if (bonuses.has(value) || value === 'damage') return false;
-	return true;
-}
-
-/**
- * Capitalizes the first letter of every other word in the input text, with
- * few exceptions, which are instead capitalized fully
- *
- * @param {String} text
- *   Text to be capitalized
- *
- * @return {String}
- *   String with alternate words in the input text capitalized, or the text
- *   fully capitalized if the text is one of several values
- */
-function capitalize(text) {
-	const fullCapWords = new Set([ // These words are fully capitalized
-		'str', 'int', 'dex', 'luk', 'cha', 'dm', 'fs',
-		'wis', 'end', 'dm', 'so', 'dc', 'da', 'ak'
-	]);
-	if (fullCapWords.has(text)) return text.toUpperCase();
-
-	if (!text || !text.trim()) return text;
-
-	return text
-		.trim()
-		.split(' ')
-		.map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
 }
 
 function tokenizeExpression(expression) {
